836f230401d88ff774aad3b47c78ca9379a79c56
Yuta Nakamura <nakamurayuta0204@gmail.com>
2017-04-06 17:02:00 -0400

    add input.py to handle input

--- data_process.py
+++ data_process.py
@@ -1,26 +1,15 @@
-
 # coding: utf-8
 
-# In[1]:
 
-import struct
 import re
 import os
 import pandas as pd
-from PIL import Image
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.model_selection import *
-
-
-# In[2]:
 
 IMAGE_HEIGHT = 127
 IMAGE_WIDTH = 128
 
-
-# In[3]:
 
 def file_name_to_label(filename):
     # to get hex value of character code
@@ -28,15 +17,14 @@ def file_name_to_label(filename):
     char = hex_label[0][2:]
     label = int(char, 16)
     # to make label start from 0
-    return label -34
+    return label - 34
 
-
-# In[4]:
 
 def binary_to_tensor(binary):
     image = tf.image.decode_png(binary, channels=1)
-    feature = tf.reshape(image, [1, IMAGE_HEIGHT,IMAGE_WIDTH, 1 ])
+    feature = tf.reshape(image, [1, IMAGE_HEIGHT, IMAGE_WIDTH, 1])
     return feature
+
 
 def file_name_to_binary(filename):
     with open(filename, "rb") as f:
@@ -44,11 +32,10 @@ def file_name_to_binary(filename):
     return binary
 
 
-# In[5]:
-
 def make_dataset(directory="data_hiragana/", shape=None):
     """
-    make data. data format is [data_size, (image + label)]. Resize data when shape is not None.
+    make data. data format is [data_size, (image + label)].
+    Resize data when shape is not None.
     
     Parrameter:
         directory: string
@@ -63,32 +50,29 @@ def make_dataset(directory="data_hiragana/", shape=None):
     # convert label to tensor
     labels = tf.constant(labels, dtype=tf.float32)
     labels = tf.reshape(labels, [-1, 1])
-    
+
     # load png data as tensor
     binaries = [file_name_to_binary(directory + x) for x in files]
     features = [binary_to_tensor(x) for x in binaries]
     # concat features 
-    features = tf.concat(features, axis =0)
+    features = tf.concat(features, axis=0)
     # resize the vector when it is not none.
     if shape is not None:
-            features = tf.image.resize_images(features, size=shape)
-    
+        features = tf.image.resize_images(features, size=shape)
+
     if shape is None:
         shape = [IMAGE_HEIGHT, IMAGE_WIDTH]
-    
+
     features = tf.cast(features, tf.float32)
     # flatted image
     features = tf.reshape(features, [-1, shape[0] * shape[1]])
-    
+
     # create data by concating features and labels
-    
+
     data = tf.concat([features, labels], axis=1)
-    
 
     return data
 
-
-# In[6]:
 
 def make_csv(data, sprit=None):
     """
@@ -109,32 +93,24 @@ def make_csv(data, sprit=None):
         print(data.shape)
     # read data as DataFrame
     Dataframe = pd.DataFrame(data)
-    
+
     # change name of column for labels
     names = Dataframe.columns.tolist()
     names[-1] = "label"
     Dataframe.columns = names
-    
+
     if sprit == None:
         # make csv
         Dataframe.to_csv("labeled_data.csv", index=False)
     else:
         # sprit the data
-        train,  test = train_test_split(Dataframe, train_size=sprit, random_state=0)
+        train, test = train_test_split(Dataframe, train_size=sprit, random_state=0)
         # write to csv
         train.to_csv("train_data.csv", index=False)
         test.to_csv("test_data.csv", index=False)
 
 
-# In[8]:
-
 if __name__ == "__main__":
     data = make_dataset(shape=(64, 64))
     print("finish loading data. now writing......")
     make_csv(data, sprit=0.2)
-
-
-
-
-
-
---
+++ input.py
@@ -0,0 +1,46 @@
+
+import pandas as pd
+
+
+class Input(object):
+    """
+    class for handling input. having two data. train and test
+    """
+
+    def __init__(self,  train_file="train_data.csv",  test_file="test_data.csv"):
+
+        self.train_data = pd.read_csv(train_file)
+        self.test_data = pd.read_csv(test_file)
+
+
+    def train_batch(self, n=100):
+        """
+        return feature and label vector
+
+        :param n:
+        :return: xs,  ys
+            array. xs is feature and ys is one hot vector of label
+        """
+
+        sample = self.train_data.sample(n=n,  axis=0)
+        print(sample.shape)
+        return sample.iloc[:, :-1], sample.iloc[:, -1]
+
+    def test_batch(self, n=100):
+        """
+        return feature and label vector
+
+        :param n:
+        :return: xs,  ys
+            array. xs is feature and ys is one hot vector of label
+        """
+
+        sample = self.test_data.sample(n=n, axis=0)
+        print(sample.shape)
+        return sample.iloc[:, :-1], sample.iloc[:, -1]
+
+
+if __name__  == "__main__":
+    Data = Input()
+    print(Data.train_batch())
+
